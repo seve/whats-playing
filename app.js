ada40154efa5e561b5da90fc636adce483018706
Severiano Badajoz <sevebadajoz@gmail.com>
2018-10-16 14:36:59 -0700

    Move spotifyAPI to song controller

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const exphbs = require('express-handlebars');
-const spotifyWebApi = require('spotify-web-api-node');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -11,10 +10,12 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.static('public'));
 
+
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -24,27 +25,13 @@ app.use(cookieParser());
 mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/whats-playing', { useNewUrlParser: true });
 
 const PORT = process.env.PORT || 3000;
-const SPOTIFY_SECRET = process.env.SPOTIFY_SECRET;
 
 const song = require('./controllers/song.js');
 const auth = require('./controllers/auth.js');
 
-const spotifyAPI = new spotifyWebApi({
-    clientId: '5bba4ba0e80648da9e557f9cddafe6cf',
-    clientSecret: SPOTIFY_SECRET
-});
+song(app);
+auth(app);
 
-spotifyAPI.clientCredentialsGrant().then(
-  (data) => {
-    console.log('The access token is ' + data.body['access_token']);
-    spotifyAPI.setAccessToken(data.body['access_token']);
-    song(app, spotifyAPI);
-    auth(app);
-  },
-  function(err) {
-    console.log('Something went wrong!', err);
-  }
-);
 
 
 
--- controllers/song.js
+++ controllers/song.js
@@ -1,13 +1,34 @@
 const Song = require('../models/song.js');
+const spotifyWebApi = require('spotify-web-api-node');
 
-module.exports = (app, spotifyAPI) => {
+const SPOTIFY_SECRET = process.env.SPOTIFY_SECRET;
+
+const spotifyAPI = new spotifyWebApi({
+    clientId: '5bba4ba0e80648da9e557f9cddafe6cf',
+    clientSecret: SPOTIFY_SECRET
+});
+
+spotifyAPI.clientCredentialsGrant().then(
+    (data) => {
+        console.log('The access token is ' + data.body['access_token']);
+        spotifyAPI.setAccessToken(data.body['access_token']);
+    },
+    function(err) {
+        console.log('Something went wrong!', err);
+    }
+);
+
+module.exports = (app) => {
     app.get('/', (req, res) => {
+        console.log(req.cookies);
         // Find all songs with global privacy, grab the songID and sort by the date created
-        Song.find({ privacy: 0 }).sort('-_id')
+        Song.find({
+                privacy: 0
+            }).sort('-_id')
             .then((data) => {
                 // With the data extract the song IDs
                 const songIDs = [];
-                for(const val of data) {
+                for (const val of data) {
                     songIDs.push(val['spotifySongID']);
                 }
                 // Grab tracks with id via spotifyAPI
@@ -37,6 +58,7 @@ module.exports = (app, spotifyAPI) => {
         spotifyAPI.getTrack(req.params.id)
             .then((data) => {
                 res.render('share-form', {
+                    userID: req.cookies,
                     song: data.body
                 });
             }, (err) => {
@@ -55,14 +77,16 @@ module.exports = (app, spotifyAPI) => {
 
     app.delete('/share/:spotifySongID', (req, res) => {
         console.log("Trying to delete song with spotifySongID:", req.params.spotifySongID);
-        Song.deleteOne({ spotifySongID: req.params.spotifySongID})
-        .then((song) => {
-            if(song.n == 1) {
-                console.log("Destroyed:", req.params.spotifySongID);
-            }
-            res.status(200).send(song);
-        }).catch((err) => {
-            res.status(400).send(err);
-        })
+        Song.deleteOne({
+                spotifySongID: req.params.spotifySongID
+            })
+            .then((song) => {
+                if (song.n == 1) {
+                    console.log("Destroyed:", req.params.spotifySongID);
+                }
+                res.status(200).send(song);
+            }).catch((err) => {
+                res.status(400).send(err);
+            })
     })
 };
