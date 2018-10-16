f1e6eb9939b97fe29d6051361de3f6ed549496cc
Severiano Badajoz <sevebadajoz@gmail.com>
2018-10-16 11:41:06 -0700

    Create User model

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    following: {
+        type: [UserSchema],
+        required: false,
+    },
+    spotifyUserID: {
+        type: String,
+        trim: true,
+        required: false,
+    }
+});
+
+UserSchema.pre('save', (next) => {
+    this.password = bcrypt.hashSync(this.password, saltRounds);
+    next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
