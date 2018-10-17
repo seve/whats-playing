fc4afbf0bbd119f513a428876033e970d0e2606e
Severiano Badajoz <sevebadajoz@gmail.com>
2018-10-17 10:56:47 -0700

    Find a user's shares and add show them in feed

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,45 @@
 const Song = require('../models/song.js');
 const User = require('../models/user.js');
+const spotifyAPI = require('../lib/spotify.js');
 
 module.exports = (app) => {
     app.get('/user/:username', (req, res) => {
-        User.findOne({ username: req.params.username })
-        .then((user) => {
-            const songsIDs = user.shares;
-            //TODO: Figure out how to manage the spotify api helper and its Credentials
-        }).catch((err) => {
-            console.error(err);
-        })
+        let currentUser = req.user;
+        User.findOne({
+                username: req.params.username
+            }).populate('shares')
+            .then((user) => {
+                const songIDs = user.shares.map(a => a.spotifySongID);
+                console.log(user.shares);
+                spotifyAPI.getTracks(songIDs)
+                    .then((songs) => {
+                        console.log("Found User's Shares:", songs);
+                        res.render('user-profile', {
+                            songs: songs.body.tracks,
+                            currentUser: currentUser,
+                            user: user,
+                            helpers: {
+                                ifEquals: function(arg1, arg2, options) {
+                                    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+                                }
+                            }
+                        });
+                    }, (err) => {
+                        console.error(err);
+                        res.render('user-profile', {
+                            songs: [],
+                            currentUser: currentUser,
+                            user: user,
+                            helpers: {
+                                ifEquals: function(arg1, arg2, options) {
+                                    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+                                }
+                            }
+                        });
+                    });
+
+            }).catch((err) => {
+                console.error(err);
+            })
     })
 }
